2d5534533033fa4e5a4402b5ecaa58faa99dd7bc
BlackNeo <webscribo@gmail.com>
2017-03-30 00:03:30 +0200

    Add files via upload

--- test_deepomatic/js/jquery.js
+++ test_deepomatic/js/jquery.js
@@ -18,13 +18,14 @@ $(document).ready(function(){
             cache: false,
             beforeSend: function(xhr){
                 xhr.setRequestHeader("Content-Type","application/x-www-form-urlencoded");
-                xhr.setRequestHeader("Content-Lenght",base64.length);
+                //xhr.setRequestHeader("Content-Length",base64.length);
             },
             headers: {
-                "Content-Type":"application/json",
+                "Content-Type":"application/x-www-form-urlencoded",
                 "x-api-key": "b01a86463f6e4d58978da77b912d7fa5",
                 "x-app-id": "283723326633",
-                //"Content-Lenght": base64.length
+                //"Content-Length": base64.length,
+                //"Authorization": "OAuth api_key=ACCESSTOKEN"
             },
             data: {                
                 "base64":base64 
@@ -37,39 +38,7 @@ $(document).ready(function(){
             alert( "Request failed: " + textStatus +"\njqXHR : " + jqXHR);
             console.log(jqXHR);
         });
-   });
+    });
    
-   /*function ajaxPOSTTest() {
-        try {
-            // Opera 8.0+, Firefox, Safari
-            ajaxPOSTTestRequest = new XMLHttpRequest();
-        } catch (e) {
-            // Internet Explorer Browsers
-            try {
-                ajaxPOSTTestRequest = new ActiveXObject("Msxml2.XMLHTTP");
-            } catch (e) {
-                try {
-                    ajaxPOSTTestRequest = new ActiveXObject("Microsoft.XMLHTTP");
-                } catch (e) {
-                    // Something went wrong
-                    alert("Your browser broke!");
-                    return false;
-                }
-            }
-        }
-
-        ajaxPOSTTestRequest.onreadystatechange = ajaxCalled_POSTTest;
-        var url = "https://api.deepomatic.com/v0.6/detect/fashion/";
-        var params = "lorem=ipsum&name=binny";
-        ajaxPOSTTestRequest.open("POST", url, true);
-        ajaxPOSTTestRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-        ajaxPOSTTestRequest.send(params);
-    }
-
-    //Create a function that will receive data sent from the server
-    function ajaxCalled_POSTTest() {
-        if (ajaxPOSTTestRequest.readyState == 4) {
-            document.getElementById("output").innerHTML = ajaxPOSTTestRequest.responseText;
-        }
-    }*/
+   
 });
