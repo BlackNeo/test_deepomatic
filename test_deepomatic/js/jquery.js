b54a71e2ab5274be089e126f3adcee51cd036f0d
BlackNeo <webscribo@gmail.com>
2017-04-01 17:56:02 +0200

    Add files via upload

--- test_deepomatic/js/jquery.js
+++ test_deepomatic/js/jquery.js
@@ -7,17 +7,19 @@
 $(document).ready(function(){
    var upload = $('img.upload');
    var result = $('.base64');
+   var statusFromServer;
    
-   upload.on('load', function(){
+    upload.on('load', function(){
         var base64 = $(this).attr('ng-src');
 	base64 = base64.replace("data:image/jpeg;base64,", "");
-	console.log(base64);
+	//console.log(base64);
         var request = $.ajax({
                 url: "https://api.deepomatic.com/v0.6/detect/fashion/",
                 type: "POST",
                 crossDomain: true,
                 dataType: 'json',
                 cache: false,
+                success : callbackTask,
                 beforeSend: function(xhr){
                     xhr.setRequestHeader("Content-Type","application/json");
                 },
@@ -26,35 +28,44 @@ $(document).ready(function(){
                     "x-api-key": "b01a86463f6e4d58978da77b912d7fa5"
                 },
                 data: '{"base64":"'+base64+'"}',
-                success: function(data) {
-//                    console.log(data);
-                            var task = JSON.stringify(data['task_id']);
-                            task = task.replace(/"/g, "");
-                                    $.ajax({
-                                    url: 'https://api.deepomatic.com/v0.6/tasks/'+task,
-                                    type: "GET",
-                                    crossDomain: true,
-                                    dataType: 'json',
-                                    beforeSend: function(xhr){
-                                        xhr.setRequestHeader("Content-Type","application/json");
-                                    },
-                                    headers: {
-                                        "x-app-id": "283723326633",
-                                        "x-api-key": "b01a86463f6e4d58978da77b912d7fa5"
-                                    },
-                                    success: function(data) {
-                                        alert('OK Status' + data);
-                                        console.log(data);
-                                    },
-                                    error : function(jqXHR, textStatus) {
-                                    alert( "Request failed: " + textStatus +"\njqXHR : " + jqXHR);
-                                }
-                            });
-                },
                 error : function(jqXHR, textStatus) {
                 alert( "Request failed: " + textStatus +"\njqXHR : " + jqXHR);
                 console.log(jqXHR);
             }
         });
-    });   
+    });
+        
+    function callbackTask(data) {
+        var task = JSON.stringify(data['task_id']);
+        task = task.replace(/"/g, "");
+                $.ajax({
+                url: 'https://api.deepomatic.com/v0.6/tasks/'+task,
+                type: "GET",
+                crossDomain: true,
+                dataType: 'json',                                    
+                success: callbackStatus,
+                beforeSend: function(xhr){
+                    xhr.setRequestHeader("Content-Type","application/json");
+                },
+                headers: {
+                    "x-app-id": "283723326633",
+                    "x-api-key": "b01a86463f6e4d58978da77b912d7fa5"
+                },
+                error : function(jqXHR, textStatus) {
+                alert( "Request failed: " + textStatus +"\njqXHR : " + jqXHR);
+            }
+        });
+    }
+            
+    function callbackStatus(statusFromServer) {
+        var status = statusFromServer.task.status;
+        var pending = "pending";
+        if (status == pending) {
+            setTimeout(function () {
+                callbackStatus(statusFromServer);
+                console.log(status);
+            }, 2000);
+        }
+        console.log(status == pending);
+    }
 });
